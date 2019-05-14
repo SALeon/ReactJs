2ee671b900a0a39e479f95da42fdfd9f6869953e
SALeon <developleon@gmail.com>
2019-05-14 08:52:50 +0300

    add animation

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -7,10 +7,25 @@ import MovieList from '../movie-list';
 import ErrorBoundary from '../error-boundary';
 import RoutMovieInfo from '../routes/route-movie-info';
 import MoviesHeader from '../movies-header';
+import MovieInfoHeader from '../movie-info-header';
+
 import { filterItems, siteLabel, searchResult, searchLabel, sortingInfo, genreInfoLabel } from '../../constants/staticData';
 import '../../styles/base.scss';
 import './app.scss';
 
+
+const routes = [
+  {
+    path: '/movies',
+    Component: MoviesHeader,
+    labels: { siteLabel, filterItems, searchLabel, searchResult, sortingInfo },
+  },
+  {
+    path: '/movies/:id',
+    Component: MovieInfoHeader,
+    labels: { siteLabel, searchLabel, genreInfoLabel },
+  },
+];
 
 const App = () => (
   <BrowserRouter>
@@ -18,39 +33,91 @@ const App = () => (
     <Provider store={store}>
       <ErrorBoundary>
         <div className="app-container">
-          <Route render={({ location }) => (
-            <TransitionGroup component={null}>
-              <CSSTransition
-                key={location.key}
-                classNames="header-"
-                timeout={4000}
-              >
-                <Switch location={location}>
-                  <Route
-                    path="/movies"
-                    render={props => (
-                      <MoviesHeader
-                        {...props}
-                        labels={{ siteLabel, filterItems, searchLabel, searchResult, sortingInfo }}
-                      />
-                    )}
-                    exact
-                  />
-                  <Route
-                    path="/movies/:id"
-                    render={props => (
-                      <RoutMovieInfo
-                        {...props}
-                        labels={{ siteLabel, searchLabel, genreInfoLabel }}
-                      />
-                    )}
-                    exact
-                  />
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          )}
-          />
+          <div className="header-container">
+            <Route render={({ location }) => (
+              <TransitionGroup component={null}>
+                <CSSTransition
+                  key={location.key}
+                  classNames="header-"
+                  timeout={6000}
+                >
+                  <Switch location={location}>
+                    <Route
+                      path="/movies"
+                      render={props => (
+                        <MoviesHeader
+                          {...props}
+                          labels={{
+                            siteLabel,
+                            filterItems,
+                            searchLabel,
+                            searchResult,
+                            sortingInfo,
+                          }}
+                        />
+                      )}
+                      exact
+                    />
+                    <Route
+                      key="/movies/:id"
+                      path="/movies/:id"
+                      render={props => (
+                        <RoutMovieInfo
+                          {...props}
+                          labels={{ siteLabel, searchLabel, genreInfoLabel }}
+                        />
+                      )}
+
+                      // render={({ match }) => {
+                      //   console.log(match);
+                      //   return (
+                      //     <CSSTransition
+                      //       in={match !== null}
+                      //       mountOnEnter
+                      //       unmountOnExit
+                      //       classNames="movie-info__header-"
+                      //       timeout={6000}
+                      //     >
+                      //       <MovieInfoHeader
+                      //         // {...props}
+                      //         childProps={{ id: match.params.id }}
+                      //         labels={{ siteLabel, searchLabel, genreInfoLabel }}
+                      //       />
+                      //     </CSSTransition>
+                      //   );
+                      // }}
+
+
+                      // {routes.map(({ path, Component, labels }) => (
+                      //   <Route
+                      //     key={path}
+                      //     exact
+                      //     path={path}
+                      //   >
+                      //     {({ match }) => (
+                      //       <CSSTransition
+                      //         in={match != null}
+                      //         timeout={6000}
+                      //         classNames="header-"
+                      //         unmountOnExit
+                      //       >
+                      //         <Component labels={labels} />
+                      //       </CSSTransition>
+                      //     )
+                      //   }
+                      //   </Route>
+                      // ))}
+
+
+                      exact
+                    />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            )}
+            />
+          </div>
+
           <main className="main">
             <MovieList />
           </main>
--- src/components/routes/route-movie-info.jsx
+++ src/components/routes/route-movie-info.jsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { CSSTransition } from 'react-transition-group';
 import MovieInfoHeader from '../movie-info-header';
 
 class RoutMovieInfo extends PureComponent {
@@ -17,10 +18,21 @@ class RoutMovieInfo extends PureComponent {
     return (<MovieInfoHeader childProps={{ id }} labels={this.props.labels} />);
   }
 
+              //     <CSSTransition
+                      //       in={match !== null}
+                      //       mountOnEnter
+                      //       unmountOnExit
+                      //       classNames="movie-info__header-"
+                      //       timeout={6000}
+                      //     >
+                      //       <MovieInfoHeader
+                      //         childProps={{ id: match.params.id }}
+                      //         labels={this.props.labels}
+                      //       />
+                      //     </CSSTransition>
+
   render() {
-    return (
-      <Route path="/movies/:id" render={this.getMovieInfo} exact />
-    );
+    return (this.getMovieInfo);
   }
 }
 export default RoutMovieInfo;
